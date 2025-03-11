7b3dfff48928258d0dee40be28c480616d1df260
Yuliscqua <git@stackblitz.com>
2025-03-11 15:22:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# API_Rest
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Yuliscqua/API_Rest)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+//Pour afficher les images : https://www.cheapshark.com{path-to-image}
+
+function App() {
+  const [website, setWebsite] = useState([]);
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+
+  fetch('https://www.cheapshark.com/api/1.0/stores', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      const ;
+    })
+    .catch((error) => console.error(error));
+  return (
+    <header>
+      <h1>Cheap Deals</h1>
+    </header>
+  );
+}
+
+export default App;
