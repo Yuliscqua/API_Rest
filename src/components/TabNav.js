c79621b80d9518392ba07c18770ef1738059f23e
Yuliscqua <m_rannou4@etu-webschoolfactory.fr>
2025-03-17 13:59:47 +0100

    Fixe le fichier TabNav -> le fichier tab était dédoublé

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,14 +1,27 @@
 import React from 'react';
-class Tab extends React.Component {
+class TabNav extends React.Component {
   render() {
-    if(this.props.isSelected) {
-      return (
-        <div>
-          {this.props.children}
-        </div>
-      );
-    }
-    return null;
+    return (
+      <div>
+        <ul className="nav nav-tabs">
+          {this.props.tabs.map((tab) => {
+            const active = tab === this.props.selected ? 'active' : '';
+            return (
+              <li className="nav-item" key={tab}>
+                <a
+                  className={'nav-link' + active}
+                  onClick={() => this.props.setSelected(tab)}
+                >
+                  {tab}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        {this.props.children}
+      </div>
+    );
   }
 }
-export default Tab;
+
+export default TabNav;
